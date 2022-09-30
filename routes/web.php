2b6233933047492ec17518d68c1236bc8ef8651d
waslacy <wallace201269wzs@gmail.com>
2022-09-30 11:35:42 -0300

    finalizado controllers

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('/')->group(function () {
+    Route::get('/', 'HomeController@index');
+
+    Route::get('/catalogo', 'CatalogoController@index');
+    Route::get('/catalogo/{slug}', 'CatalogoController@show');
+    
+    Route::get('/produto/{slug}', 'ProdutoController@index');
 });
