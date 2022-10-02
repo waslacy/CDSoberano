fa5724e9fd5302e6a4f3e82be12c1f36fe2bd3f1
waslacy <wallace201269wzs@gmail.com>
2022-10-01 21:56:57 -0300

    tabelas criadas / falta popular

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Soberano</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/default/default.css">
+    <link rel="stylesheet" href="css/home/home.css">
+    <link rel="stylesheet" href="css/catalog/catalog.css">
+    <link rel="stylesheet" href="css/product/product.css">
 </head>
 <body>
     <header id="site-header">
@@ -35,6 +39,6 @@
         </div>
     </footer>
 
-    <script src=""></script>
+    <script src="js/app.js"></script>
 </body>
 </html>
