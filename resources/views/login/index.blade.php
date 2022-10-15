81620ec85423c5fcb9cf2efe21a5bf0827c50382
Waslacy <wallace201269wzs@gmail.com>
2022-10-15 00:12:18 -0300

    finalizado

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -5,29 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Soberano Painel</title>
+    <link rel="stylesheet" href="css/login/login.css">
 </head>
 <body>
-    <form action="/auth" method="post">
+    <form action="/auth" method="post" class="login">
+        <h1>Login</h1>
         @csrf
-        <input type="text" name="email">
-        <input type="password" name="password">
+        <label for="email">E-mail</label>
+        <input type="text" placeholder="E-mail" name="email">
+        <label for="password">Senha</label>
+        <input type="password" placeholder="Senha" name="password">
         <button type="submit">enviar</button>
+
+        @if ($errors->any())
+            <div class="fieldsFail">
+                <ul>
+                    @foreach ($errors->all() as $e)
+                        <li>{{$e}}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        @if (session('danger'))
+            <div class="authFail">
+                <h4>{{session('danger')}}</h4>
+            </div>
+        @endif
     </form>
-
-    @if ($errors->any())
-        <div class="fieldsFail">
-            <ul>
-                @foreach ($errors->all() as $e)
-                    <li>{{$e}}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-    @if (session('danger'))
-        <div class="authFail">
-            <h4>{{session('danger')}}</h4>
-        </div>
-    @endif
 </body>
 </html>
